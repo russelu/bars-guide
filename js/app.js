b02c3335f736519c08a64f735c9ef691c29abeb6
russelu <30449569+russelu@users.noreply.github.com>
2017-10-13 18:01:57 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,24 +1,60 @@
 var Place = function(data) {
-	this.lat = ko.observable(data.lat);
-	this.lng = ko.observable(data.lng);
+	this.location = ko.observable(data.geometry.location);
+
 };
 
-var places = [
+var default_places = [
 	{
-		lat: 0,
-		lng: 0,
-		name: 'Sample'
+		"geometry" : {
+            "location" : {
+               "lat" : 42.2791584,
+               "lng" : -83.7484618
+            }
+        },
+		"name": "Jolly Pumpkin Café & Brewery"
+	},
+	{
+		"geometry" : {
+            "location" : {
+               "lat" : 42.28030979999999,
+               "lng" : -83.74780079999999
+            }
+        },
+        "name" : "Arbor Brewing Company Brewpub"
+	},
+	{
+		"geometry" : {
+            "location" : {
+               "lat" : 42.2803524,
+               "lng" : -83.74944139999999
+            }
+        },
+        "name" : "Frita Batidos"
+	},
+	{
+		"geometry" : {
+            "location" : {
+               "lat" : 42.2806313,
+               "lng" : -83.7494193
+            }
+        },
+        "name" : "Grizzly Peak Brewing Co."
+	},
+	{
+		"geometry" : {
+            "location" : {
+               "lat" : 42.279399,
+               "lng" : -83.74892299999999
+            }
+        },
+        "name" : "Black Pearl Ann Arbor"
 	}
 ]
 
 var ViewModel = function() {
 	var self = this;
 	
-	this.placeList = ko.observableArray([]);
-
-	places.forEach(function(place){
-		self.placeList.push(new Place(place))
-	});
+	
 	
 };
 
